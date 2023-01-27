86c4ce7feef844b928de65c8ca1de5e36a5e5543
Belza <belzamp@hotmail.com>
2023-01-27 09:35:43 -0300

    primer repositorio

---
+++ function.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick = function () {
+    console.log("formulario enviado");
+    document.getElementById("respuesta").innerHTML = "Gracias por escribirme, pronto me pondré en contacto contigo!"
+}
